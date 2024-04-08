c621f4256ffb1eb80adc3d2a241700fa421018b6
Nicolas Paul <n@nc0.fr>
2024-04-09 00:35:31 +0200

    connect4: add gravity and rotations
    Signed-off-by: Nicolas Paul <n@nc0.fr>

--- src/main/java/fr/nc0/cda/puissance4/modele/Puissance4.java
+++ src/main/java/fr/nc0/cda/puissance4/modele/Puissance4.java
@@ -8,17 +8,18 @@ package fr.nc0.cda.puissance4.modele;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /** Représente une partie de Puissance 4. */
 public class Puissance4 {
-  /** La longueur de la grille */
-  private final int longueur;
+  /** La longueur de la grille, c'est-à-dire le nombre de colonnes */
+  private int longueur;
 
-  /** La hauteur de la grille */
-  private final int hauteur;
+  /** La hauteur de la grille, c'est-à-dire le nombre de lignes */
+  private int hauteur;
 
   /** La grille de jeu. */
-  private final List<List<CellulePuissance4>> grille;
+  private List<List<CellulePuissance4>> grille;
 
   /** L'état de la partie */
   private EtatPartiePuissance4 etat = EtatPartiePuissance4.EN_COURS;
@@ -31,11 +32,26 @@ public class Puissance4 {
     if (hauteur < 1) throw new IllegalArgumentException("La hauteur doît être supérieure à 0");
     this.hauteur = hauteur;
 
-    grille = new ArrayList<>();
-    for (int i = 0; i < this.longueur; i++) {
-      grille.add(new ArrayList<>());
-      for (int j = 0; j < this.hauteur; j++) grille.get(i).add(CellulePuissance4.VIDE);
+    this.grille = creerGrille(longueur, hauteur);
+  }
+
+  /**
+   * Crée une grille de jeu vide.
+   *
+   * @param longueur la longueur de la grille
+   * @param hauteur la hauteur de la grille
+   * @return la grille de jeu vide
+   */
+  private List<List<CellulePuissance4>> creerGrille(int longueur, int hauteur) {
+    List<List<CellulePuissance4>> grid = new ArrayList<>(longueur);
+
+    for (int i = 0; i < longueur; ++i) {
+      List<CellulePuissance4> ligne = new ArrayList<>(hauteur);
+      for (int j = 0; j < hauteur; ++j) ligne.set(j, CellulePuissance4.VIDE);
+      grid.set(i, ligne);
     }
+
+    return grid;
   }
 
   /**
@@ -264,6 +280,106 @@ public class Puissance4 {
       for (int j = 1; j <= hauteur; j++) actualiserEtatPartie(i, j);
   }
 
+  /**
+   * Rotationne la grille de jeu de 90° dans le sens horaire ou anti-horaire, selon la rotation
+   * donnée.
+   *
+   * <p>La rotation est effectuée en appliquant une matrice de rotation sur la grille. Aucune
+   * gravité n'est appliquée. Il peut aussi y avoir des pertes de données si la grille n'est pas
+   * carrée, ou au contraire des données pouvant être dupliquées.
+   *
+   * @param rotation la rotation à effectuer
+   */
+  private void rotationnerGrille(RotationPuissance4 rotation) {
+    int nouvelleLongueur = hauteur;
+    int nouvelleHauteur = longueur;
+    List<List<CellulePuissance4>> grilleRotationnee =
+        creerGrille(nouvelleLongueur, nouvelleHauteur); // On inverse les dimensions
+
+    // Note : commencer à 1 au lieu de 0 pour suivre la formule mathématique
+    // qui indexe à 1.
+    for (int i = 1; i <= longueur; ++i) {
+      for (int j = 1; j <= hauteur; ++j) {
+        // Rotation de 90° dans le sens horaire d'une matrice 3x2 vers une
+        // matrice 2x3 :
+        //
+        //                  ┌────────────┐     Matrice M        Matrice M'
+        //               ┌──┘┌──────────┐└┐    d'origine :      d'arrivée :
+        //           ┌───┼───┘┌────────┐└┐│
+        //       ┌───┼───┼────┘ ┌──┬──┐└┐││      * A -> 1,1       * A' -> 1,2
+        //     ┌─┴─┬─┴─┬─┴─┐  ┌►│D'│A'│◄┘││      * B -> 1,2       * B' -> 2,2
+        //     │ A │ B │ C │ ┌┘ ├──┼──┤ ┌┘│      * C -> 1,3       * C' -> 3,2
+        //     ├───┼───┼───┤┌┘┌►│E'│B'│◄┘┌┘      * D -> 2,1       * D' -> 1,1
+        //     │ D │ E │ F ││┌┘ ├──┼──┤ ┌┘       * E -> 2,2       * E' -> 2,1
+        //     └─┬─┴─┬─┴─┬─┘││┌►│F'│C'│◄┘        * F -> 2,3       * F' -> 3,1
+        //       └┐  └┐  └──┼┼┘ └──┴──┘
+        //        └┐  └─────┼┘                 ∀ɣ∈M, ∀ɣ'∈M, ɣ'₁ = ɣ₂,
+        //         └────────┘                               ɣ'₂ = n - ɣ₁
+        if (Objects.requireNonNull(rotation) == RotationPuissance4.HORAIRE)
+          grilleRotationnee.get(j).set(hauteur - i, grille.get(i).get(j));
+
+        // Rotation de 90° dans le sens horaire inverse d'une matrice 3x2 vers
+        // une matrice 2x3 :
+        //
+        //                  ┌────────────┐     Matrice M        Matrice M'
+        //               ┌──┘┌──────────┐└┐    d'origine :      d'arrivée :
+        //           ┌───┼───┘┌────────┐└┐│
+        //       ┌───┼───┼────┘ ┌──┬──┐└┐││      * A -> 1,1       * A' -> 3,1
+        //     ┌─┴─┬─┴─┬─┴─┐  ┌►│C'│F'│◄┘││      * B -> 1,2       * B' -> 2,1
+        //     │ A │ B │ C │ ┌┘ ├──┼──┤ ┌┘│      * C -> 1,3       * C' -> 1,1
+        //     ├───┼───┼───┤┌┘┌►│B'│E'│◄┘┌┘      * D -> 2,1       * D' -> 3,2
+        //     │ D │ E │ F ││┌┘ ├──┼──┤ ┌┘       * E -> 2,2       * E' -> 2,2
+        //     └─┬─┴─┬─┴─┬─┘││┌►│A'│D'│◄┘        * F -> 2,3       * F' -> 1,2
+        //       └┐  └┐  └──┼┼┘ └──┴──┘
+        //        └┐  └─────┼┘                 ∀ɣ∈M, ∀ɣ'∈M, ɣ'₁ = n - ɣ₂,
+        //         └────────┘                               ɣ'₂ = ɣ₁
+        //
+        else if (rotation == RotationPuissance4.ANTI_HORAIRE)
+          grilleRotationnee.get(longueur - j).set(i, grille.get(i).get(j));
+      }
+    }
+
+    grille = grilleRotationnee;
+    longueur = nouvelleLongueur;
+    hauteur = nouvelleHauteur;
+  }
+
+  /** Applique la gravité sur la grille de jeu, en déplaçant les cellules vides vers le bas. */
+  private void appliquerGravite() {
+    // Pour simuler la gravité, il nous suffit pour chaque colonne, d'insérer
+    // les cellules non vides dans une file, puis d'y ajouter
+    // hauteur - len(file) cases vides.  On défile la file dans l'ordre pour
+    // insérer dans la colonne.
+
+    // Faire une copie de la grille actuelle nous permet de réinitialiser la
+    // grille existante et de profiter des fonctions pré-définies pour insérer
+    // un jeton.
+    List<List<CellulePuissance4>> copieGrille = new ArrayList<>(grille);
+    grille = creerGrille(longueur, hauteur);
+
+    List<CellulePuissance4> file = new ArrayList<>();
+    for (int colonne = 1; colonne <= hauteur; ++colonne) {
+      int tailleFile = 0; // Évite d'appeler file.size() qui requière une boucle
+
+      // On enfile uniquement les jetons colorés dans une file.
+      for (int ligne = 1; ligne <= longueur; ++ligne) {
+        CellulePuissance4 cellule = copieGrille.get(ligne).get(colonne);
+        if (cellule != null && cellule != CellulePuissance4.VIDE) {
+          file.add(cellule);
+          ++tailleFile;
+        }
+      }
+
+      // On comble l'espace restant de vide
+      if (tailleFile < hauteur)
+        for (int k = 0; k < tailleFile; ++k) file.add(CellulePuissance4.VIDE);
+
+      // Reste qu'à insérer nos jetons dans la colonne.
+      for (CellulePuissance4 cellule : file) insererCellule(colonne, cellule);
+      file.clear(); // Permet de réutiliser la file pour la prochaine colonne
+    }
+  }
+
   // ===========================================================================
   // Public API
   // ===========================================================================
@@ -317,5 +433,20 @@ public class Puissance4 {
 
     int ligne = insererCellule(colonne, jeton);
     actualiserEtatPartie(colonne, ligne);
+  }
+
+  /**
+   * Rotationne la grille de jeu de 90° dans le sens horaire ou anti-horaire.
+   *
+   * @param rotation la rotation à effectuer
+   * @throws IllegalStateException si la partie est terminée
+   */
+  public void rotationner(RotationPuissance4 rotation) {
+    if (etat != EtatPartiePuissance4.EN_COURS)
+      throw new IllegalStateException("La partie est terminée");
+
+    rotationnerGrille(rotation);
+    appliquerGravite();
+    actualiserEtatPartie();
   }
 }
