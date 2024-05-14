52f775569abaacc4419575e497bab7e5b7106a2d
Nicolas Paul <n@nc0.fr>
2024-05-14 23:09:29 +0200

    Create game specific Choix input classes
    Signed-off-by: Nicolas Paul <n@nc0.fr>

---
+++ src/main/java/fr/nc0/cda/modele/nim/ChoixNim.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024 Lucas Paulo, Younes Ouaammou, Nicolas Paul.
+ * Use of this source code is governed by a BSD-style license
+ * that can be found in the COPYRIGHT file.
+ */
+
+package fr.nc0.cda.modele.nim;
+
+import fr.nc0.cda.modele.Choix;
+
+/** Les choix possibles pour jouer au jeu de Nim. */
+public class ChoixNim extends Choix {
+  /** Le tas dans lequel jouer */
+  private final int tas;
+
+  /** Le nombre d'allumettes à retirer */
+  private final int allumettes;
+
+  ChoixNim(int tas, int allumettes) {
+    this.tas = tas;
+    this.allumettes = allumettes;
+  }
+
+  /** Le tas dans lequel jouer */
+  public int getTas() {
+    return tas;
+  }
+
+  /** Le nombre d'allumettes à retirer */
+  public int getAllumettes() {
+    return allumettes;
+  }
+}
---
+++ src/main/java/fr/nc0/cda/modele/puissance4/ChoixPuissance4.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 Lucas Paulo, Younes Ouaammou, Nicolas Paul.
+ * Use of this source code is governed by a BSD-style license
+ * that can be found in the COPYRIGHT file.
+ */
+
+package fr.nc0.cda.modele.puissance4;
+
+import fr.nc0.cda.modele.Choix;
+
+public class ChoixPuissance4 extends Choix {
+  /** Le coup du joueur */
+  private final CoupPuissance4 coup;
+
+  /** Le sens de rotation du joueur, si le coup est une rotation */
+  private final Rotation rotation;
+
+  /** La colonne dans laquelle jouer, si le coup est une insertion */
+  private final int colonne;
+
+  ChoixPuissance4(CoupPuissance4 coup, Rotation rotation, int colonne) {
+    super();
+    this.coup = coup;
+    this.rotation = rotation;
+    this.colonne = colonne;
+  }
+
+  /**
+   * Le coup que le joueur veut effectuer.
+   *
+   * @return le coup
+   */
+  public CoupPuissance4 getCoup() {
+    return coup;
+  }
+
+  /**
+   * Le sens de rotation
+   *
+   * @return le sens de rotation
+   */
+  public Rotation getRotation() {
+    return rotation;
+  }
+
+  /**
+   * La colonne dans laquelle il faut insérer.
+   *
+   * @return la colonne
+   */
+  public int getColonne() {
+    return colonne;
+  }
+}
