3c7c5311220e74ee961902e27909882eff63ce56
Nicolas Paul <n@nc0.fr>
2024-05-14 23:09:00 +0200

    Model games with abstract classes
    Signed-off-by: Nicolas Paul <n@nc0.fr>

---
+++ src/main/java/fr/nc0/cda/modele/Choix.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024 Lucas Paulo, Younes Ouaammou, Nicolas Paul.
+ * Use of this source code is governed by a BSD-style license
+ * that can be found in the COPYRIGHT file.
+ */
+
+package fr.nc0.cda.modele;
+
+/**
+ * Choix est une classe qui représente les inputs des utilisateurs en les stockants dans une liste à
+ * taille non déterminée (un jeu peu avoir plusieurs inputs requis), et laisse à chaque jeu la
+ * possibilité de fournir des méthodes permettant de récupérer ces inputs aec un type.
+ */
+public abstract class Choix {}
---
+++ src/main/java/fr/nc0/cda/modele/Plateau.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 Lucas Paulo, Younes Ouaammou, Nicolas Paul.
+ * Use of this source code is governed by a BSD-style license
+ * that can be found in the COPYRIGHT file.
+ */
+
+package fr.nc0.cda.modele;
+
+/** Le plateau du jeu en cours. */
+public abstract class Plateau {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
